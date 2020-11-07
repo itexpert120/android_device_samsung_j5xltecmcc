60d5f4499540bc16052813d596026c4dd76f436c
itexpert120 <itexpert120@gmail.com>
2020-11-07 15:51:17 +0100

    j5xltecmcc: updates

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,5 @@
 LOCAL_PATH := device/samsung/j5xltecmcc
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/omni_j5xltecmcc.mk \
-	$(LOCAL_DIR)/rr_j5xltecmcc.mk \
 	$(LOCAL_DIR)/lineage_j5xltecmcc.mk
 
 COMMON_LUNCH_CHOICES := \
--- system_prop.mk
+++ system_prop.mk
@@ -2,11 +2,6 @@
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.qualcomm.cabl=2
 
-# RAM
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.config.low_ram=false \
-	ro.config.zram=false
-
 # RIL
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	telephony.lteOnCdmaDevice=0 \
